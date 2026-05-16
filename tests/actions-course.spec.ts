ea7b68580bb1e8540b12094515b7f790c0036ecb
Elena <rlenka2010gmail.com>
2026-05-16 15:41:31 +0300

    Ex2

--- tests/actions-course.spec.ts
+++ tests/actions-course.spec.ts
@@ -21,4 +21,24 @@ test.describe('Form Layouts page', () => {
     await expect(emailInput).toHaveValue('john@test.com');
     await expect(emailInput).not.toBeFocused();
   });
+
+//ex.2.
+test('radio-group - action', async ({ page }) => {
+  const usingTheGrid = page.locator('nb-card', {hasText: 'Using the Grid'});
+  const firstOption = usingTheGrid.getByRole('radio', {name: 'Option 1'});
+  const secondOption = usingTheGrid.getByRole('radio', {name: 'Option 2'});
+  const disabledOption = usingTheGrid.getByRole('radio', {name: 'Disabled Option'});
+  
+await firstOption.check({ force: true });
+await expect(firstOption).toBeChecked();
+await expect(secondOption).not.toBeChecked();
+await expect(disabledOption).toBeDisabled();
+
+await secondOption.check({ force: true }); 
+await expect(secondOption).toBeChecked();
+await expect(firstOption).not.toBeChecked();
+await expect(disabledOption).not.toBeChecked();
+await expect(disabledOption).toBeDisabled();
+ 
+});
 });
