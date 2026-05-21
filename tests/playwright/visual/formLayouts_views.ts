a94b7ed2792df16a9243357b804a7bbecc376a72
Elena <rlenka2010gmail.com>
2026-05-21 18:19:07 +0200

    argos added

---
+++ tests/playwright/visual/formLayouts_views.ts
@@ -0,0 +1,19 @@
+import {test} from '@playwright/test';
+import {argosFullScreenshot, argosComponentScreenshot} from '../support/utils/argosSmartScreenShot';
+
+test.describe('Form Layouts', () => {
+    test('Form Layouts - full page view', async ({page, onApplicationURLs}) => {
+        await onApplicationURLs.navigateToFormsLayout();
+        await argosFullScreenshot({page, snapshotName: 'Form Layouts - full page view'});
+    });
+      test('Form Layouts - Inline Form block', async ({ page, onApplicationURLs, onInlineForm }) => {
+    await onApplicationURLs.navigateToFormsLayouts();
+    await onInlineForm.assertVisibility(true);
+
+    await argosComponentScreenshot({
+      page,
+      snapshotName: 'Form Layouts - Inline Form block',
+      selector: onInlineForm.card,
+    });
+  });
+});
